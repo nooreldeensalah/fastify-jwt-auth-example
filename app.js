c547cafa033608cabf04760950568e72635e740f
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-09-29 22:31:09 +0200

    Install and register fastify-mongodb, fastify-env plugins

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const AutoLoad = require('fastify-autoload')
+const path = require("path");
+const AutoLoad = require("fastify-autoload");
 
+/**
+ *
+ * @param {import("fastify").FastifyInstance} fastify
+ * @param {*} opts
+ */
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
+
+  fastify.register(require("fastify-env"), {
+    dotenv: true,
+    schema: {
+      type: "object",
+      required: ["MONGODB_URI"],
+      properties: {
+        MONGODB_URI: {
+          type: "string",
+        },
+      },
+    },
+  });
+
+  fastify.register(require("fastify-mongodb"), {
+    forceClose: true, // Force close the database connection when the app is stopped.
+    url: process.env.MONGODB_URI,
+  });
 
   // Do not touch the following lines
 
@@ -12,14 +35,14 @@ module.exports = async function (fastify, opts) {
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
+};
