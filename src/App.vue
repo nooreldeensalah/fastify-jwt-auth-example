dfd8f090f0dda5d328b0d37e0f6fa6e52209d647
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-10-01 08:09:54 +0200

    Persist login after page reload

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,20 @@
 
 <script lang="ts">
 import NavigationBar from "@/components/NavigationBar.vue";
+import { defineComponent } from "vue";
 
-export default {
+export default defineComponent({
   name: "App",
   components: {
     NavigationBar,
   },
-};
+  created() {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      this.$store.commit("saveToken", accessToken);
+    }
+  },
+});
 </script>
 
 <style>
