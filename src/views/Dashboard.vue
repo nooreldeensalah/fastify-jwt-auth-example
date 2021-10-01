b39a06e6be614cb4e9bc1f67535220f46b327d34
Noor Eldeen Salah <m-2123325228@s-mu.edu.eg>
2021-10-01 19:25:22 +0200

    Add HTML/CSS for the dashboard table

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="content">
     <div v-if="!isLoading">
-      <div class="user" v-for="user in users" :key="user.username">
-        {{ user }}
-      </div>
+      <h2>Users Table</h2>
+      <table id="users">
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Hash</th>
+        </tr>
+        <tr v-for="user in users" :key="user._id">
+          <td>{{ user._id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.username }}</td>
+          <td>{{ user.passwordHash }}</td>
+        </tr>
+      </table>
     </div>
     <h2 v-else-if="!this.$store.state.accessToken">
       You must be authenticated in order to access the dashboard
@@ -33,3 +45,33 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#users {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#users td,
+#users th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#users tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+#users tr:hover {
+  background-color: #ddd;
+}
+
+#users th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04aa6d;
+  color: white;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,14 @@
     <img alt="Vue logo" src="../assets/logo.png" />
 
     <div v-if="!this.$store.state.accessToken">
-      To access the dashboard you will need to
-      <router-link to="/login">Login</router-link>
-      or
-      <router-link to="/register">Register</router-link>
+      <h2>
+        To access the dashboard you will need to
+        <router-link to="/login">Login</router-link>
+        or
+        <router-link to="/register">Register</router-link>
+      </h2>
     </div>
+    <h2 v-else>You can access the dashboard now</h2>
   </div>
 </template>
 
